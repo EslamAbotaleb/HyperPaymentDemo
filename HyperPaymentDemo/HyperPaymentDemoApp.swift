46ec7946c9b772783c0fa9e4461f0644a0bcc7bc
Eslam Baiomy <ebaiomy@youxel.com>
2025-09-08 15:44:13 +0300

    Initial Commit

---
+++ HyperPaymentDemo/HyperPaymentDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HyperPaymentDemoApp.swift
+//  HyperPaymentDemo
+//
+//  Created by Eslam on 08/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct HyperPaymentDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
